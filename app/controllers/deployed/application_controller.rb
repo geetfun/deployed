bb22de8c1594222bfe112349d361292664a5bcaa
Simon Chiu <simon@furvur.com>
2023-10-24 14:03:12 -0400

    Move some methods around

--- app/controllers/deployed/application_controller.rb
+++ app/controllers/deployed/application_controller.rb
@@ -14,5 +14,46 @@ module Deployed
     def initialize_deployed
       Deployed.setup!
     end
+
+    def lock_file_path
+      Rails.root.join(Deployed::DIRECTORY, 'process.lock')
+    end
+
+    def lock_process
+      File.open(lock_file_path, 'a') do |file|
+        file.puts(Deployed::CurrentExecution.child_pid)
+      end
+    end
+
+    def release_process
+      return unless File.exist?(lock_file_path)
+
+      File.delete(lock_file_path)
+    end
+
+    def stored_pid
+      return false unless File.exist?(lock_file_path)
+
+      value = File.read(lock_file_path).to_i
+
+      if value.is_a?(Integer)
+        value
+      else
+        false
+      end
+    end
+
+    def process_running?
+      return false unless stored_pid
+
+      begin
+        # Send signal 0 to the process to check if it exists
+        Process.kill(0, stored_pid)
+        true
+      rescue Errno::ESRCH
+        false
+      end
+    end
+    helper_method :process_running?
   end
 end
